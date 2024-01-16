636b0bc2634bea83083ad9bfe5c14e306e784740
Gabriel Carvalho <155925978+GabrielCarvalho0812@users.noreply.github.com>
2024-01-16 13:47:28 -0300

    Add files via upload

---
+++ Estrutura Repeticao 2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cc = 1;
+        do {
+            System.out.println("cambalhota " + cc);
+              cc++;
+        }while (cc<=4);
+    }
+}
---
+++ Estrutura Repeticao 3/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int cc = 0; cc <=10 ;cc +=2){ // += mesma coisa de: cc = cc + 2
+            System.out.println(cc );
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        for (int mm = 15; mm >=5; mm -=2){ //-= mesma coisa de : mm = mm-2
+            System.out.println(mm);
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        for (int ff = 0; ff <=100; ff +=10){ // += mesma coisa de ff = ff +10
+            System.out.println(ff);
+        }
+
+
+
+
+
+
+    }
+}
